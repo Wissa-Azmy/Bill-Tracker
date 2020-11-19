5801d722e6e05c9c06bf54e7c5b5d7cb88926681
Wissa Michael <wissa@neugelb.com>
2020-11-20 00:16:32 +0100

    Initial Commit

---
+++ Discounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Discounter
+//
+//  Created by Wissa Michael on 20.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Discounter/DiscounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscounterApp.swift
+//  Discounter
+//
+//  Created by Wissa Michael on 20.11.20.
+//
+
+import SwiftUI
+
+@main
+struct DiscounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
